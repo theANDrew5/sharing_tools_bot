dec2c1a3bde2a85f62db4719d253a8afff75d13f
andrew <andrey.nik.999@gmail.com>
2021-02-24 15:49:32 +0700

    Made repository open.

--- Models/AppSettings.cs
+++ Models/AppSettings.cs
@@ -1,12 +1,13 @@
 ﻿//настройки
 
+
 namespace tel_bot_net.Models
 {
     public static class AppSettings
     {
-        public static string Url { get; set; }  = "https://e1ff6ce919bb.ngrok.io:443{0}";
+        public static string Url { get; set; }  = "https://5abb61fec13a.ngrok.io:443{0}";
         public static string Name { get; set; } = "Tools Sharing Bot";
-        public static string Key { get; set; }  = "1680835335:AAE0z-105C2nmqV6LPjnsjZFDkGs4uVWjfw";
+        public static string Key { get; set; }
         
     }
 }
